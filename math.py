d741c22e2e9f75369b7332c79cc52f229b66f3e9
aniketagrawal05 <64480342+aniketagrawal05@users.noreply.github.com>
2021-07-02 14:21:27 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+# Add implementation
+def add(x,y):
+	pass
+# Sub implementation
+def sub(x,y):
+	pass
+# Multiply implementation
+def multiple(x,y):
+	pass
+# Divide implementation
+def divide(x,y):
+	pass
