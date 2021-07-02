76a8b2bcf21fac6839db2ce3a9d4a6c30eb16e16
Aniket <aniket_agrawal@persistent.com>
2021-07-02 14:31:01 +0530

    Added function headers for add and subtract

--- math.py
+++ math.py
@@ -1,12 +1,12 @@
 # Add implementation
 def add(x,y):
 	pass
-# Sub implementation
+# Subtract implementation
 def sub(x,y):
 	pass
-# Multiply implementation
+
 def multiple(x,y):
 	pass
-# Divide implementation
+
 def divide(x,y):
 	pass
